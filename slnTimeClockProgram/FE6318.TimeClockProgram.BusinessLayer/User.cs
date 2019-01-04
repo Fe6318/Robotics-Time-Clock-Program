36012c6c06a38c0ec8c4ec41dc4fc40fc9d778c9
Hayden <500153494@fvtc.edu>
2019-01-04 13:52:14 -0600

    Got rid of saving stuff in the user class

--- slnTimeClockProgram/FE6318.TimeClockProgram.BusinessLayer/User.cs
+++ slnTimeClockProgram/FE6318.TimeClockProgram.BusinessLayer/User.cs
@@ -23,6 +23,9 @@ namespace FE6318.TimeClockProgram.BusinessLayer
         private string userID;
         public string UserID { get => userID; set => userID = value; }
         
+        /// <summary>
+        /// TODO: Fix
+        /// </summary>
         public double LoggedHours
         {
             //calculate the logged hours using the log files
@@ -51,10 +54,6 @@ namespace FE6318.TimeClockProgram.BusinessLayer
 
         private DateTime timeClockedOut;
         public DateTime TimeClockedOut { get => timeClockedOut; set => timeClockedOut = value; }
-
-        private string logDirectory;
-
-        private String userDirectory;
         
 
         public User(String userID, String firstName, String lastName)
@@ -63,28 +62,7 @@ namespace FE6318.TimeClockProgram.BusinessLayer
             FirstName = firstName;
             LastName = lastName;
 
-            logDirectory = Environment.CurrentDirectory + @"\Information\Log";
-
-            //create a directory for the user
-            System.IO.Directory.CreateDirectory(this.logDirectory + @"\" + firstName + lastName);
-
-            //create the log files
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(this.logDirectory + @"\" + firstName + lastName + @"\in.6318", true)) { }
-
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(this.logDirectory + @"\" + firstName + lastName + @"\out.6318", true)) { }
-
-            if(System.IO.File.ReadLines(logDirectory + @"\" + firstName + lastName + @"\in.6318").Count() > System.IO.File.ReadLines(logDirectory + @"\" + firstName + lastName + @"\out.6318").Count())
-            {
-                isClockedIn = true;
-                timeClockedIn = DateTime.Parse(System.IO.File.ReadLines(logDirectory + @"\" + firstName + lastName + @"\in.6318").Skip(System.IO.File.ReadLines(logDirectory + @"\" + firstName + lastName + @"\in.6318").Count() - 1).Take(1).First());
-            } else
-            {
-                isClockedIn = false;
-            }
-
-            userDirectory = Environment.CurrentDirectory + @"\Information\Users\" + FirstName + LastName;
+            
         }
 
         
@@ -95,7 +73,6 @@ namespace FE6318.TimeClockProgram.BusinessLayer
                 return false;
             }
             timeClockedIn = DateTime.Now;
-            login(timeClockedIn);
             isClockedIn = true;
             return true;
         }
@@ -120,7 +97,6 @@ namespace FE6318.TimeClockProgram.BusinessLayer
 
             timeClockedOut = DateTime.Now;
             isClockedIn = false;
-            logOut(timeClockedOut);
             return true;
         }
 
@@ -136,64 +112,10 @@ namespace FE6318.TimeClockProgram.BusinessLayer
             {
                 timeClockedOut = DateTime.Now;
                 isClockedIn = false;
-                logOut(timeClockedOut);
-                logTimes(timeClockedIn, timeClockedOut);
-                saveHours(timeClockedIn, timeClockedOut);
             } else
             {
                 timeClockedIn = DateTime.Now;
-                login(timeClockedIn);
                 isClockedIn = true;
-            }
-        }
-
-        private void logTimes(DateTime cIn, DateTime cOut)
-        {
-            //log when the users clocked in and out
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(logDirectory + @"\log.txt" , true))
-            {
-                file.WriteLine(Name + " Clocked In: " + cIn.ToShortDateString() +  " " + cIn.ToLongTimeString() + " Clocked Out: " + cOut.ToShortDateString() + cOut.ToLongTimeString());
-            }
-        }
-
-        private void login(DateTime cIn)
-        {
-            //log when the users clocked in
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(logDirectory + @"\" + FirstName + LastName + @"\in.6318", true))
-            {
-                file.WriteLine(cIn.ToString());
-            }
-        }
-
-        private void logOut(DateTime cOut)
-        {
-            //log when the users clocked out
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(logDirectory + @"\" + FirstName + LastName + @"\out.6318", true))
-            {
-                file.WriteLine(cOut.ToString());
-            }
-        }
-
-        
-
-        private void saveHours(DateTime cIn, DateTime cOut)
-        {
-            //store the current user directory
-            String strCurrentUserDirectory = userDirectory + @"\" + firstName + lastName + ".6318";
-
-            //write to the file the number of hours worked
-            using (System.IO.StreamWriter file =
-                new System.IO.StreamWriter(strCurrentUserDirectory, false)) //false means it will overwrite the current file
-            {
-                //not possible to write to a certain line of a file (as far as I know) So we have to re-write the whole file
-                file.WriteLine(userID);
-                file.WriteLine(firstName);
-                file.WriteLine(lastName);
-                //now we finally can write the number of hours worked
-                file.WriteLine((LoggedHours).ToString());
             }
         }
     }
