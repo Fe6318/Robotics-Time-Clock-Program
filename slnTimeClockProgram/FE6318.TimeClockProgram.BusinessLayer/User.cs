a443f062abef74f5b865f52197118eee59f375ba
Hayden <500153494@fvtc.edu>
2019-01-21 15:14:36 -0600

    Added documentation to User class

--- slnTimeClockProgram/FE6318.TimeClockProgram.BusinessLayer/User.cs
+++ slnTimeClockProgram/FE6318.TimeClockProgram.BusinessLayer/User.cs
@@ -7,22 +7,38 @@ using System.IO;
 
 namespace FE6318.TimeClockProgram.BusinessLayer
 {
-   /*
-    * Keeps track of a user.
-    */
+    /// <summary>
+    /// Keep tracks of a User
+    /// Contains all methods relating to a User
+    /// </summary>
     public class User
     {
         private string firstName;
+        /// <value>
+        /// Gets or sets the users first name
+        /// </value>
         public string FirstName { get => firstName; set => firstName = value; }
 
         private string lastName;
+        /// <value>
+        /// Gets or sets the users last name
+        /// </value>
         public string LastName { get => lastName; set => lastName = value; }
 
+        /// <value>
+        /// Returns the user's formatted name
+        /// </value>
         public string Name { get => FirstName + " " + LastName; }
 
         private string userID;
+        /// <value>
+        /// Gets or sets the User's UserID
+        /// </value>
         public string UserID { get => userID; set => userID = value; }
         
+        /// <value>
+        /// Gets the number of hours a user has been clocked in
+        /// </value>
         public double LoggedHours
         {
             //calculate the logged hours using the log files
@@ -38,15 +54,30 @@ namespace FE6318.TimeClockProgram.BusinessLayer
         }
 
         private bool isClockedIn;
+        /// <value>
+        /// Gets or sets wether or not a user is clocked in
+        /// </value>
         public bool IsClockedIn { get => isClockedIn; set => isClockedIn = value; }
 
         private List<DateTime> timeClockedIn;
+        /// <value>
+        /// Gets or sets a list of every time the user has clocked in
+        /// </value>
         public List<DateTime> TimeClockedIn { get => timeClockedIn; set => timeClockedIn = value; }
 
         private List<DateTime> timeClockedOut;
+        /// <value>
+        /// Gets or sets a list of every time the user has clocked out
+        /// </value>
         public List<DateTime> TimeClockedOut { get => timeClockedOut; set => timeClockedOut = value; }
         
-
+        /// <summary>
+        /// Sets <paramref name="userID"/>, <paramref name="firstName"/>, and <paramref name="lastName"/> respectively.
+        /// Also initializes list of times clocked in and list of times clocked out
+        /// </summary>
+        /// <param name="userID">The user's ID</param>
+        /// <param name="firstName">The user's first name</param>
+        /// <param name="lastName">The user's last name</param>
         public User(String userID, String firstName, String lastName)
         {
             UserID = userID;
@@ -56,12 +87,19 @@ namespace FE6318.TimeClockProgram.BusinessLayer
             timeClockedOut = new List<DateTime>();
         }
 
+        /// <summary>
+        /// Initializes list of times clocked in and list of times clocked out
+        /// </summary>
         public User()
         {
             timeClockedIn = new List<DateTime>();
             timeClockedOut = new List<DateTime>();
         }
-        
+
+        /// <summary>
+        /// Clock the user in
+        /// </summary>
+        /// <returns>Wether or not the user was clocked in</returns>
         public bool clockIn()
         {
             if(isClockedIn)
@@ -73,17 +111,29 @@ namespace FE6318.TimeClockProgram.BusinessLayer
             return true;
         }
 
+        /// <summary>
+        /// Gets the number of hours between the last times clocked in and out
+        /// </summary>
+        /// <returns>Number of hours between last clocks in and out</returns>
         public double getNumberOfHoursElapsedBetweenClocks()
         {
             TimeSpan tsTimeWorked = timeClockedOut[timeClockedOut.Count - 1].Subtract(timeClockedIn[timeClockedIn.Count - 1]);
             return tsTimeWorked.TotalHours;
         }
 
+        /// <summary>
+        /// Gets the last clock in time
+        /// </summary>
+        /// <returns>The last clock in time</returns>
         public String getFormatedClockInTime()
         {
             return timeClockedIn[timeClockedIn.Count - 1].ToShortDateString() + " " + timeClockedIn[timeClockedIn.Count - 1].ToLongTimeString();
         }
 
+        /// <summary>
+        /// Clocks the user in
+        /// </summary>
+        /// <returns>Wether or not the user was clocked out</returns>
         public bool clockOut()
         {
             if(!isClockedIn)
@@ -96,11 +146,18 @@ namespace FE6318.TimeClockProgram.BusinessLayer
             return true;
         }
 
+        /// <summary>
+        /// Gets the last clock out time formatted
+        /// </summary>
+        /// <returns>The last clock out time formatted</returns>
         public String getFormatedClockOutTime()
         {
             return timeClockedOut[timeClockedOut.Count - 1].ToShortDateString() + " " + timeClockedOut[timeClockedOut.Count - 1].ToLongTimeString();
         }
 
+        /// <summary>
+        /// Clocks in the user if they are not clocked in, or clocks out the user if they are clocked in
+        /// </summary>
         public void clock()
         {
             //if clocked out, clock in
