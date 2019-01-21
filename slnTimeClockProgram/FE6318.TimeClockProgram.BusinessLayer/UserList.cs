e189210cad5d5d6e7295fbf2504bb17801eb253a
Hayden <500153494@fvtc.edu>
2019-01-21 15:16:35 -0600

    Added documentation to UserList class

--- slnTimeClockProgram/FE6318.TimeClockProgram.BusinessLayer/UserList.cs
+++ slnTimeClockProgram/FE6318.TimeClockProgram.BusinessLayer/UserList.cs
@@ -7,14 +7,23 @@ using System.Threading.Tasks;
 
 namespace FE6318.TimeClockProgram.BusinessLayer
 {
+    /// <summary>
+    /// Adds save and read functionality to  the List class
+    /// </summary>
     public class UserList : List<User>
     {
+        /// <summary>
+        /// Saves all the users in the list
+        /// </summary>
         public void Save()
         {
             XML xml = new XML(Environment.CurrentDirectory + @"\Information\UserList.6318");
             xml.SerializeList<User>((List<User>)this);
         }
 
+        /// <summary>
+        /// Reads all of the saved users then adds them to the list
+        /// </summary>
         public void Read()
         {
             this.Clear();
